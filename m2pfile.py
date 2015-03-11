0b49d01ed05695a4b8ad3a60da029d72ea5bff08
Vita Loginova <vita.loginova@gmail.com>
2015-03-11 22:54:56 +0300

    Add MyFileLoader class for legacy mybuild files

---
+++ m2pfile.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.insert(0, "../mybuild")
+
+from _compat import *
+
+from nsloader import pyfile
+from m2p_parser import my_parse
+
+class MyFileLoader(pyfile.PyFileLoader):
+    """Loads My-files using myfile parser/linker."""
+
+    def defaults_for_module(self, module):
+        return dict(self.defaults,
+                    __my_module__=module)
+
+    def _exec_module(self, module):
+        source_string = self.get_source(self.name)
+        res = my_parse(source_string)
+        module.__dict__.update(res)
+
