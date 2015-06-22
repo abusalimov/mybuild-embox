a881013fa94731d8be4e55162c4561b94c4751d2
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2015-06-22 22:04:01 +0300

    lex: Add legacy ^escape notation for identifiers

--- mybuild_embox/lang_legacy/lex.py
+++ mybuild_embox/lang_legacy/lex.py
@@ -83,8 +83,9 @@ t_E_WILDCARD = r'\.\*'
 
 # Identifiers
 def t_ID(t):
-    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    r'\^?[a-zA-Z_][a-zA-Z_0-9]*'
     t.type = reserved.get(t.value,'ID')    # Check for reserved words
+    t.value = t.value.lstrip('^')
     if iskeyword(t.value):
         t.value += '_'
     return t
